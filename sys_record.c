a22b29e766ccc89a5b05e4d90639b2dc008aec40
Alan Newton <rufeooo@gmail.com>
2019-10-03 13:44:40 -0700

    Extend recordings to allow for independent read/write

--- sys_record.c
+++ sys_record.c
@@ -1,4 +1,5 @@
 
+#include <stdio.h>
 #include <string.h>
 
 #include "macro.h"
@@ -8,16 +9,17 @@ typedef struct Record_s {
   char *restrict buf;
   int allocBuf;
   int usedBuf;
-  bool playback;
+  int writeOffset;
+  int readOffset;
 } Record_t;
 
 Record_t *
-sys_recordInit()
+sys_recordAlloc()
 {
-  Record_t *rec = calloc(sizeof(Record_t), 1);
+  Record_t *rec = malloc(sizeof(Record_t));
   rec->usedBuf = 0;
+  rec->readOffset = rec->writeOffset = 0;
   rec->allocBuf = 4 * 1024;
-  free(rec->buf);
   rec->buf = malloc(rec->allocBuf);
 
   return rec;
@@ -42,35 +44,88 @@ sys_recordRealloc(Record_t *rec, int bytesNeeded)
 bool
 sys_recordAppend(Record_t *rec, size_t len, const char *input)
 {
-  if (rec->playback)
-    return false;
-  if (!sys_recordRealloc(rec, (len + rec->usedBuf + 1) - rec->allocBuf))
+  size_t needed = len + rec->writeOffset + 1;
+  if (!sys_recordRealloc(rec, needed - rec->allocBuf))
     return false;
 
-  memcpy(rec->buf + rec->usedBuf, input, len);
-  rec->usedBuf += len;
-  rec->buf[rec->usedBuf++] = 0;
+  memcpy(rec->buf + rec->writeOffset, input, len);
+  rec->writeOffset += len;
+  rec->buf[rec->writeOffset++] = 0;
+  rec->usedBuf = MAX(rec->usedBuf, rec->writeOffset);
 
   return true;
 }
 
 void
-sys_recordPlayback(Record_t *rec, sys_recordEvent handler)
+sys_recordPlaybackAll(Record_t *rec, sys_recordEvent handler)
 {
-  rec->playback = true;
   size_t length = { 0 };
   for (int i = 0; i < rec->usedBuf; i += (length + 1)) {
     length = strlen(rec->buf + i);
     handler(length, rec->buf + i);
   }
-  rec->playback = false;
+}
+
+bool
+sys_recordPlayback(Record_t *rec, sys_recordEvent handler)
+{
+  if (rec->readOffset >= rec->usedBuf)
+    return false;
+
+  size_t length = strlen(&rec->buf[rec->readOffset]);
+  handler(length, &rec->buf[rec->readOffset]);
+  rec->readOffset += length + 1;
+
+  return true;
 }
 
 void
-sys_recordShutdown(Record_t *rec)
+sys_recordSeekR(Record_t *rec, size_t nth)
 {
-  rec->usedBuf = 0;
-  rec->allocBuf = 0;
+  int i = 0;
+  for (; nth > 0 && i < rec->usedBuf; ++i) {
+    if (rec->buf[i] == 0) {
+      --nth;
+    }
+  }
+
+  rec->readOffset = i;
+}
+
+void
+sys_recordSeekW(Record_t *rec, size_t nth)
+{
+  int i = 0;
+  for (; nth > 0 && i < rec->usedBuf; ++i) {
+    if (rec->buf[i] == 0) {
+      --nth;
+    }
+  }
+
+  rec->writeOffset = i;
+}
+
+Record_t *
+sys_recordClone(Record_t *rec)
+{
+  Record_t *clone = malloc(sizeof(Record_t));
+  *clone = *rec;
+  clone->buf = malloc(clone->allocBuf);
+  memcpy(clone->buf, rec->buf, rec->usedBuf);
+  return clone;
+}
+
+void
+sys_recordFree(Record_t *rec)
+{
   FREE(rec->buf);
   FREE(rec);
 }
+
+void
+sys_recordDebug(Record_t *rec)
+{
+  printf("%p: %d alloc %d used %d wo %d ro\n", rec, rec->allocBuf,
+         rec->usedBuf, rec->writeOffset, rec->readOffset);
+}
+
--- sys_record.h
+++ sys_record.h
@@ -7,7 +7,13 @@
 typedef void (*sys_recordEvent)(size_t strlen, char *str);
 typedef struct Record_s Record_t;
 
-Record_t *sys_recordInit();
+Record_t *sys_recordAlloc();
 bool sys_recordAppend(Record_t *rec, size_t len, const char *input);
-void sys_recordPlayback(Record_t *rec, sys_recordEvent handler);
-void sys_recordShutdown(Record_t *rec);
+void sys_recordPlaybackAll(Record_t *rec, sys_recordEvent handler);
+bool sys_recordPlayback(Record_t *rec, sys_recordEvent handler);
+void sys_recordSeekW(Record_t *rec, size_t nth);
+void sys_recordSeekR(Record_t *rec, size_t nth);
+Record_t *sys_recordClone(Record_t *rec);
+void sys_recordFree(Record_t *rec);
+
+void sys_recordDebug(Record_t *rec);
