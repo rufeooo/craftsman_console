c269234a726a4d65823d377bf024562be8dc51cd
Alan Newton <rufeooo@gmail.com>
2019-10-29 08:59:53 -0700

    Modify visibility of record to be local

--- record.h
+++ record.h
@@ -10,23 +10,27 @@ typedef struct {
   uint32_t command_count;
 } RecordOffset_t;
 
+#ifndef LOCAL
+#define LOCAL __attribute__ ((visibility ("hidden"))) 
+#endif
+
 typedef void (*RecordEvent_t)(size_t strlen, char *str);
 typedef struct Record_s Record_t;
 
-Record_t *record_alloc();
-bool record_append(Record_t *rec, size_t len, const char *input,
-                   RecordOffset_t *write_offset);
-bool record_can_playback(const Record_t *rec,
-                         const RecordOffset_t *read_offset);
-bool record_playback(const Record_t *rec, RecordEvent_t handler,
-                     RecordOffset_t *read_offset);
-void record_playback_all(const Record_t *rec, RecordEvent_t handler);
-Record_t *record_clone(const Record_t *rec);
-void record_reset(Record_t *rec);
-void record_free(Record_t *rec);
+LOCAL Record_t *record_alloc();
+LOCAL bool record_append(Record_t *rec, size_t len, const char *input,
+                          RecordOffset_t *write_offset);
+LOCAL bool record_can_playback(const Record_t *rec,
+                                const RecordOffset_t *read_offset);
+LOCAL bool record_playback(const Record_t *rec, RecordEvent_t handler,
+                            RecordOffset_t *read_offset);
+LOCAL void record_playback_all(const Record_t *rec, RecordEvent_t handler);
+LOCAL Record_t *record_clone(const Record_t *rec);
+LOCAL void record_reset(Record_t *rec);
+LOCAL void record_free(Record_t *rec);
 
-size_t record_length(Record_t *rec);
-int record_compare(const Record_t* lhs, const Record_t* rhs);
+LOCAL size_t record_length(Record_t *rec);
+LOCAL int record_compare(const Record_t *lhs, const Record_t *rhs);
 
-void record_debug(Record_t *rec);
+LOCAL void record_debug(Record_t *rec);
 
