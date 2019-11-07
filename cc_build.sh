1b8ded7c57c2fbe06cd377ad4b3166676ce6bb88
Alan Newton <rufeooo@gmail.com>
2019-11-07 14:44:45 -0800

    Split cc_build.sh from run.sh

---
+++ cc_build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if [ -z $CC ]; then
+  CC=`which clang`
+fi
+if [ -z $CC ]; then
+  CC=`which gcc`
+fi
+echo building with $CC
+time $CC main.c notify.c input.c dlfn.c record.c stats.c functor.c network.c hash.c -ldl -lpthread -g -O0 -ffp-contract=off 
--- run.sh
+++ run.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
-if [ -z $CC ]; then
-  CC=`which clang`
-fi
-if [ -z $CC ]; then
-  CC=`which gcc`
-fi
-echo building with $CC
-time $CC main.c notify.c input.c dlfn.c record.c stats.c functor.c network.c hash.c -ldl -lpthread -g -O0 -ffp-contract=off 
+sh ./cc_build.sh
 if [ $? -eq 0 ]; then
   LD_LIBRARY_PATH=. ./a.out $@
 else
