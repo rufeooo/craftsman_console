43a29e34e0fbca7ea3cdaba5185feedea774709a
Alan Newton <rufeooo@gmail.com>
2019-11-27 15:03:51 -0800

    Cpp .ycm config

---
+++ third_party/.ycm_extra_conf.py
@@ -0,0 +1,11 @@
+import os
+
+project_root = os.path.abspath(os.path.dirname(__file__))
+
+# YouCompleteMe default project settings
+def Settings( **kwargs ):
+  return {
+      'flags': ['-O0', "-std=c++17"],
+      'include_paths_relative_to_dir': project_root,
+      }
+
